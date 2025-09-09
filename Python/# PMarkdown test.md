7ec103a7ad7430fdd5c86bfd89349ad6ba75aaf8
sergeierbin <sergei.erbin@gmail.com>
2025-09-09 09:32:48 +0300

    Python day

---
+++ Python/# PMarkdown test.md
@@ -0,0 +1,10 @@
+# PMarkdown test
+
+- [x] Kaardista probleem ja küsimused
+- [x] Kirjelda vajalikud andmed vastuste leidmiseks
+- [x] Meisterda andmemudel
+- [x] Korja andmed
+- [x] Kontrolli andmete kvaliteet
+- [x] Tee andmebaas
+- [x] Tee analüüsid
+- [x] Koosta raport
--- Python/ex4.py
+++ Python/ex4.py
@@ -1,40 +1,63 @@
-## Funkstioonid
-## Funkstioon väite tõesuse kontrollimiseks
+# paigalda pip install tools.api_request
 
-## Loo fukstioon nimega is_adult, mis võtab parameetriks age täisarvu
-## Lisa fingimuslasuse, mis kontrollib kui sisendparameeter on 18 või suurem, siis väljastab tõenäärtuse True, vastasel juhul False
-## Testi terminalis funktsiooni väärtust funktsiooni andes sisendiks erinevaid väärtusi
-## Lisa funkstiooni kontroll, mis kontrollib kas muutuja on arv ja ei ole negatiivne arv. Kui tuvastatakse viga siis tagastatab funktsioon False
-## Testi terminalis funktsiooni väärtust andes funktsiooni sisendiks erinevaid väärtusi
-## Loo tingimuslause, mis kasutab vanuse kontrollimiseks funkstiooni is_adult. Kui tõene siis tagastab terminali "Võib Valida" vastasel juhul "Ei või valida".
-
-def is_adult(age: int) -> bool:
-    """Kontrollib kas vanus on 18 või suurem ja mitte negatiivne arv."""
-    return age >=18
-print(is_adult(20))
-print(is_adult(17))
-
-age = 20
-if is_adult(age):
-    print("Võib Valida")
-else:
-    print("Ei või valida")
-
-## Funkstioon väärtuse olemasolu kontrollimiseks loendis
-## Lisa loend eu_northen_country_codes sõnedega DK, EE, FI, IS, LT, LV, NO, SE
-## Lisa funktsioon is_northen_country (mis võtab vastu 2 kohalise riigi koodi country_code) ja tagastab kas tõenäärtuse True kui riigi kood on loetelus. vastasel juhul tagastab False.
-## Funktsioon peab tagastama alati väärtuse False, kui sisend ei ole kahetäheline string
-## Testi terminalis funktsiooni väljundit andes funktsiooni sisendiks erinevaid väärtusi
-
-eu_northen_country_codes = ["DK", "EE", "FI", "IS", "LT", "LV", "NO", "SE"]
-def is_northen_country(country_code: str) -> bool:
-    """Kontrollib kas riigi kood on Põhjamaade hulgas."""
-    if not isinstance(country_code, str) or len(country_code) != 2:
-        return False
-    return country_code.upper() in eu_northen_country_codes
+import requests
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from path import Path
 
 
-print(is_northen_country("EE"))
-print(is_northen_country("US"))
-print(is_northen_country("Est"))
-print(is_northen_country(12))
+# url = "https://demo-datahub.rik.ee/api/v1/meta/classifications"
+
+url_riik = 'https://api.worldbank.org/v2/countries/EST/?format=json'
+url_rahvastik_ = 'https://api.worldbank.org/v2/country/EST/indicator/SP.POP.TOTL?format=json'
+url_women = "https://api.worldbank.org/v2/country/EST/indicator/SP.POP.TOTL.FE.IN?format=json"
+
+response = requests.get(url_rahvastik_)
+data = response.json()
+
+response_woman = requests.get(url_women)
+data_woman = response_woman.json()
+
+
+# json dumps muudab väljundi terminalis loetavaks
+# print(json.dumps(data, indent=2, ensure_ascii=False))
+
+values = {'year': [], 'population': [], 'women_population': []}
+
+# {"year": ["2021", "2020", "2019", ...], "population": [1331057, 1326535, 1324820, ...]}
+
+for item in data[1]:
+    values['year'].append(item['date'])
+    values['population'].append(item['value'])
+
+for women in data_woman[1]:
+    values['women_population'].append(women['value'])
+
+# print(json.dumps(values, indent=2, ensure_ascii=False))
+
+df = pd.DataFrame(values)
+
+# väljastan esimesed read
+print(df.head())
+df = df.sort_values(by='year')
+df['men_population'] = df['population'] - df['women_population']
+# joonistamise osa
+df.plot(x='year', y=['population', 'women_population', 'men_population'], kind='line', marker='o',
+        title='Eesti rahvaarv aastatel 1960-2021', xlabel='Aasta', ylabel='Inimeste arv')
+
+# ...existing code...
+
+plt.ylim(bottom=0)
+os.makedirs('output', exist_ok=True)
+output_path = os.path.join('output', 'rahvastik_plot.png')
+plt.savefig(output_path)
+plt.show()
+# ...existing code...
+
+
+# print(df)
+
+# response_df = pd.json_normalize(data)
+# print(response_df)
